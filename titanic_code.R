6f246ec8aabf504f0e1767480a6133727a93d4f4
lgadsden <lgadsden@email.gwu.edu>
2015-04-15 19:00:32 -0400

    small updates

--- titanic_code.R
+++ titanic_code.R
@@ -20,16 +20,25 @@ titanic.train <- read.csv(trainingData,colClasses = train.column.types,
                           na.strings = missing.types)
 titanic.test <- read.csv(testData,colClasses = test.column.types,
                          na.strings = missing.types)
-#Examine Data 
+# make all columns lower case
+names(titanic.train) <- tolower(names(titanic.train))
+names(titanic.test) <- tolower(names(titanic.test))
+names(titanic.train)
 dim(titanic.train) #check dimensions
 head(titanic.train) #look at first 5 rows
 titanic.train[1:5,] # subset
 tail(titanic.train) #subset
+
 titanic.train[1:5,c(4,5,10)] 
-titanic.train$PassengerId
 titanic.train[,1]
-titanic.train[[1]]
+titanic.train[[1]] # subsets column of dataframe --- section of a list for list 
+titanic.train$passengerid # hit tab autocompletes
+
 str(titanic.train) # looks at structure of data 
 summary(titanic.train)
 
-
+table(titanic.train$survived)
+table(titanic.train$survived,titanic.train$pclass)
+table(titanic.train$survived,titanic.train$sex)
+plot(table(titanic.train$survived,titanic.train$sex), col = titanic.train$survived)
+plot(table(titanic.train$pclass, titanic.train$survived), col = titanic.train$survived)
